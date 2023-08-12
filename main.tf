a696cdf4cb209228edd372bc8d0284054d1c4702
elgwhoppo <joeman113@gmail.com>
2023-08-12 10:18:44 -0400

    add minecraft port

--- main.tf
+++ main.tf
@@ -78,6 +78,17 @@ resource "azurerm_network_security_group" "tf2_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+    security_rule {
+    name                       = "minecraft"
+    priority                   = 1005
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "25565"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create TF2 network interface
